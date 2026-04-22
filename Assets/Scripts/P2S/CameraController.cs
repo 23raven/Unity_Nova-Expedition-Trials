468c4e769d95841164ed052ef411c24040e46cb9
23raven <23raven.games@gmail.com>
2026-04-22 21:37:59 +0500

    add UI

--- Assets/Scripts/P2S/CameraController.cs
+++ Assets/Scripts/P2S/CameraController.cs
@@ -3,32 +3,64 @@
 public class CameraController : MonoBehaviour
 {
     public Transform target;
+    public float followSpeed = 5f;
 
     private Vector3 localOffset;
-    
-    public float desiredDistance = 4f;
+    private Camera cam;
 
-    public void SetTarget(Transform newTarget)
+    private Rect targetRect;
+    private bool isResizing = false;
+
+    void Awake()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    public void SetTarget(Transform newTarget, float distance = 4f)
     {
         target = newTarget;
 
-        // направление от планеты к камере
         Vector3 direction = (transform.position - target.position).normalized;
+        localOffset = direction * distance;
 
-        // задаём НОВЫЙ offset с нужной дистанцией
-        localOffset = direction * desiredDistance;
+        // 🎯 задаём целевой viewport (центр экрана)
+        targetRect = new Rect(0.2f, 0.4f, 0.6f, 0.4f);
+        isResizing = true;
     }
-
-
 
     void LateUpdate()
     {
-        if (target == null) return;
+        if (target != null)
+        {
+            Vector3 desiredPosition = target.TransformPoint(localOffset);
 
-        // преобразуем обратно в мир
-        Vector3 desiredPosition = target.TransformPoint(localOffset);
+            transform.position = Vector3.Lerp(
+                transform.position,
+                desiredPosition,
+                Time.deltaTime * followSpeed
+            );
 
-        transform.position = desiredPosition;
-        transform.LookAt(target);
+            transform.LookAt(target);
+        }
+
+        if (isResizing)
+        {
+            cam.rect = LerpRect(cam.rect, targetRect, Time.deltaTime * 3f);
+
+            if (Mathf.Abs(cam.rect.width - targetRect.width) < 0.01f)
+            {
+                isResizing = false;
+            }
+        }
+    }
+
+    Rect LerpRect(Rect a, Rect b, float t)
+    {
+        return new Rect(
+            Mathf.Lerp(a.x, b.x, t),
+            Mathf.Lerp(a.y, b.y, t),
+            Mathf.Lerp(a.width, b.width, t),
+            Mathf.Lerp(a.height, b.height, t)
+        );
     }
 }
--- Assets/Scripts/P2S/PlanetManager.cs
+++ Assets/Scripts/P2S/PlanetManager.cs
@@ -1,13 +1,16 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlanetManager : MonoBehaviour
 {
     public string planetName;
     public CameraController cameraController;
-
+    public GameObject UICanvas;
     void OnMouseDown()
     {
         Debug.Log("You tapped on planet " + planetName);
         cameraController.SetTarget(transform);
+
+        UICanvas.SetActive(true);
     }
 }
