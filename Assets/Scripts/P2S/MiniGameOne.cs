96c2ea68f107b8fdb99e0d65046ba29390028d2c
23raven <23raven.games@gmail.com>
2026-04-23 23:56:58 +0500

    add MiniGameOne

---
+++ Assets/Scripts/P2S/MiniGameOne.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MiniGameOne : MonoBehaviour
+{
+    public RectTransform horizontalBlack;
+    public RectTransform horizontalGreen;
+    public RectTransform verticalGreen;
+
+    public float speed = 2f;
+
+    private float t;
+
+    void Update()
+    {
+        MoveIndicator();
+        CheckInput();
+    }
+
+    void MoveIndicator()
+    {
+        t += Time.deltaTime * speed;
+
+        float pingPong = Mathf.PingPong(t, 1f);
+
+        // границы движения (по X)
+        float minX = horizontalBlack.position.x - horizontalBlack.rect.width / 2;
+        float maxX = horizontalBlack.position.x + horizontalBlack.rect.width / 2;
+
+        float x = Mathf.Lerp(minX, maxX, pingPong);
+
+        verticalGreen.position = new Vector3(
+            x,
+            verticalGreen.position.y,
+            verticalGreen.position.z
+        );
+    }
+
+    void CheckInput()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            CheckHit();
+        }
+    }
+
+    void CheckHit()
+    {
+        float indicatorX = verticalGreen.position.x;
+
+        float minX = horizontalGreen.position.x - horizontalGreen.rect.width / 2;
+        float maxX = horizontalGreen.position.x + horizontalGreen.rect.width / 2;
+
+        if (indicatorX >= minX && indicatorX <= maxX)
+        {
+            Debug.Log("ПОПАЛ ✅");
+        }
+        else
+        {
+            Debug.Log("ПРОМАЗАЛ ❌");
+        }
+    }
+}
