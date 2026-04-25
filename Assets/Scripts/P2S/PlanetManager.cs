b76b69960c8b28a445637c3b01dd00722fda7e79
23raven <23raven.games@gmail.com>
2026-04-25 21:57:50 +0500

    Update planetManager

--- Assets/Scripts/P2S/PlanetManager.cs
+++ Assets/Scripts/P2S/PlanetManager.cs
@@ -6,11 +6,13 @@ public class PlanetManager : MonoBehaviour
     public string planetName;
     public CameraController cameraController;
     public GameObject UICanvas;
+    public GameObject MiniGame;
     void OnMouseDown()
     {
         Debug.Log("You tapped on planet " + planetName);
         cameraController.SetTarget(transform);
 
         UICanvas.SetActive(true);
+        MiniGame.SetActive(true);
     }
 }
