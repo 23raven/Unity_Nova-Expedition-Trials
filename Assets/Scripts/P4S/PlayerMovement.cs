665880a6dba7281719bb049efe7e8ab964995280
23raven <23raven.games@gmail.com>
2026-05-05 20:40:27 +0500

    add basic level design for phase4

--- Assets/Scripts/P4S/PlayerMovement.cs
+++ Assets/Scripts/P4S/PlayerMovement.cs
@@ -3,12 +3,27 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
+    public float sprintMultiplier = 2f;
+
+    public float maxSprintTime = 3f;     // максимум ускорения (сек)
+    public float recoverySpeed = 1f;     // скорость восстановления
+
     public Transform cameraTransform;
+
+    private float currentSprintTime;
+    private bool isSprinting;
+
+    void Start()
+    {
+        currentSprintTime = maxSprintTime;
+    }
 
     void Update()
     {
-        float h = Input.GetAxis("Horizontal"); // A/D
-        float v = Input.GetAxis("Vertical");   // W/S
+        HandleSprint();
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
 
         Vector3 forward = cameraTransform.forward;
         Vector3 right = cameraTransform.right;
@@ -18,12 +33,40 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 move = forward * v + right * h;
 
-        transform.position += move * speed * Time.deltaTime;
+        float currentSpeed = speed;
 
-        // поворот в сторону движения
+        if (isSprinting)
+        {
+            currentSpeed *= sprintMultiplier;
+        }
+
+        transform.position += move * currentSpeed * Time.deltaTime;
+
         if (move != Vector3.zero)
         {
             transform.forward = move;
+        }
+    }
+
+    void HandleSprint()
+    {
+        bool shift = Input.GetKey(KeyCode.LeftShift);
+
+        if (shift && currentSprintTime > 0f)
+        {
+            isSprinting = true;
+            currentSprintTime -= Time.deltaTime;
+        }
+        else
+        {
+            isSprinting = false;
+
+            // восстановление
+            if (currentSprintTime < maxSprintTime)
+            {
+                currentSprintTime += Time.deltaTime * recoverySpeed;
+                currentSprintTime = Mathf.Clamp(currentSprintTime, 0f, maxSprintTime);
+            }
         }
     }
 }
